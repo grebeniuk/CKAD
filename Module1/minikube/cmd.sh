99cc5dbfa50994db09697aa3f2bd4c6731ce4d9f
Andriy Grebenyuk <andrew.grebeniuk@gmail.com>
2024-06-05 03:11:46 +0000

    Lesson4

---
+++ Module1/minikube/cmd.sh
@@ -0,0 +1,6 @@
+curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+sudo install minikube-linux-amd64 /usr/local/bin/minikube
+minikube start --vm-driver=docker
+minikube status
+
+kubectl completion -h
